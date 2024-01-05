b69973b078caab06992ec72601892c7c98abd8ac
Ignacio Muzio <ignaciomuzio1115@gmail.com>
2024-01-05 11:15:57 -0300

    Subject class defined

--- src/main/java/com/imuzio/crud2/model/entity/Subject.java
+++ src/main/java/com/imuzio/crud2/model/entity/Subject.java
@@ -1,4 +1,30 @@
 package com.imuzio.crud2.model.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "subjects")
 public class Subject {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotEmpty
+    @Column(unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "subject", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    List <StudentSubject> students;
 }
