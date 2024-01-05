fb253b37e90b3ad879c412cacf05711a0df3c30a
Ignacio Muzio <ignaciomuzio1115@gmail.com>
2024-01-05 11:12:41 -0300

    Student class defined

--- src/main/java/com/imuzio/crud2/model/entity/Student.java
+++ src/main/java/com/imuzio/crud2/model/entity/Student.java
@@ -1,4 +1,40 @@
 package com.imuzio.crud2.model.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "students")
 public class Student {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    @Column(name="last_name")
+    private String lastName;
+
+    @NotEmpty
+    @Column(unique = true)
+    @Size(min = 10,max = 10)
+    private String dni;
+
+    @OneToMany (mappedBy ="student", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<StudentSubject> subjects;
 }
