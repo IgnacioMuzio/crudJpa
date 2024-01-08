f8352abacc88b65125f54d9185d2ca63c9ef7807
Ignacio Muzio <ignaciomuzio1115@gmail.com>
2024-01-08 11:17:53 -0300

    Minor changes in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO students (id,first_name,last_name,dni) VALUES (1,'Nacho','Muzio','42678588');
 INSERT INTO students (id,first_name,last_name,dni) VALUES (2,'Melanie','Ziegler','43678588');
-INSERT INTO subjects (id,name) VALUES (1,'Matematica 1');
-INSERT INTO subjects (id,name) VALUES (2,'Base de datos 1');
+INSERT INTO subjects (id,name) VALUES (1,'Math 1');
+INSERT INTO subjects (id,name) VALUES (2,'Lab 1');
 INSERT INTO student_subject (id,grade,student_id,subject_id) VALUES (1,8,1,1);
